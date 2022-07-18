9c98937bded2b44008d1f92cc2a26a8aef19c544
Simon Love <simon-love@hotmail.co.uk>
2022-07-18 15:01:52 +0100

    init

--- variables-2.js
+++ variables-2.js
@@ -6,16 +6,25 @@
   Insert "console.log(name);" in the program below so that it prints out "inner"
 */
 
-// let  name = "outer";
-// function print() {
-//   let name = "inner"; 
-// }
-// print();
+//  let  name = "outer";
+//  function print() {
+//    let name = "inner"; 
+//    console.log(name);
+//  }
+//  print();
+ 
 
 /*
   Exercise 2
   Without removing the "console.log" from the program above, make it log "outer" by removing exactly 1 line
 */
+
+// let  name = "outer";
+// function print() {
+  
+//   console.log(name);
+// }
+// print();
 
 /*
   Exercise 3
@@ -23,9 +32,20 @@
 */
 
 // function outerFunction() {
-  
+//   let name = 'SCOPE'
 //   function innerFunction() {
 //     console.log(name);
+//   }
+//   innerFunction();
+// }
+// outerFunction();
+
+// // OR YOU CAN
+
+// function outerFunction() {
+  
+//   function innerFunction() {
+//     console.log(name = 'SCOPE');
 //   }
 //   innerFunction();
 // }
@@ -37,14 +57,15 @@
 */
 
 // function scope1() {
-  
+
+//   var name = "Beyonce";
+
 //   console.log(name);
 //   function scope2() {
     
 //     console.log(name);
 //     function scope3() {
-      
-//       var name = "Beyonce";
+            
 //       console.log(name);
 //     }
 //     scope3();
@@ -59,16 +80,16 @@
 */
 
 // let namesList = ["John", "Annie"];
-// function run() {
-//   let namessList = ["Ann", "Porter"];
-//   let toFind = "Ann";
-//   let capitals = namesList.forEach((name) => {
-//     if(name === toFind) {
-//       console.log("Found Ann!");
-//     }
-//   });
-// }
-// run();
+function run() {
+  let namesList = ["Ann", "Porter"];
+  let toFind = "Ann";
+  namesList.forEach((name) => {
+    if(name === toFind) {
+      console.log("Found Ann!");
+    }
+  });
+}
+run();
 
 
 
